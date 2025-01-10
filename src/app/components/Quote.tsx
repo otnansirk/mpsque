d83a3442f6f4981e59fecf499e6c4692bc223a54
otnansirk <iam.otnansirk@gmail.com>
2025-01-10 19:48:31 +0700

    feat : quote

---
+++ src/app/components/Quote.tsx
@@ -0,0 +1,12 @@
+import { ShieldExclamationIcon } from '@heroicons/react/24/outline'
+
+export default function Quote() {
+    return (
+        <div className="text-center bg-yellow-600 py-8 mt-20">
+            <div className="row flex justify-center items-center gap-4">
+                <ShieldExclamationIcon className="w-8 h-8 sm:block hidden"/>
+                <h1 className="text-2xl font-semibold">Every Muslim Needs To Realise The Importance Of The Pillar Of Islam</h1>
+            </div>
+        </div>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,13 @@
+import Features from "./components/features";
 import Hero from "./components/hero";
+import Quote from "./components/Quote";
 
 export default function Home() {
   return (
     <div className="">
       <Hero/>
+      <Features/>
+      <Quote/>
     </div>
   );
 }
