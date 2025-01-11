16b7d8fe6963aadd3c59952e8efa818d2951fd1f
otnansirk <iam.otnansirk@gmail.com>
2025-01-11 21:23:41 +0700

    fix : report and dashboard

--- src/app/components/report.tsx
+++ src/app/components/report.tsx
@@ -1,69 +1,199 @@
-import { ArrowUpCircleIcon, ArrowDownCircleIcon } from '@heroicons/react/20/solid'
-import { JSX } from 'react'
+'use client'
 
-const icon: Record<string, JSX.Element> = {
-    out: <ArrowUpCircleIcon className='w-8 h-8 text-red-400' />,
-    in: <ArrowDownCircleIcon className='w-8 h-8 text-green-300' />,
+import { ArrowUpCircleIcon, ArrowDownCircleIcon, TrashIcon } from '@heroicons/react/20/solid'
+import { JSX, useEffect, useState } from 'react'
+
+const dateFormat = (timestamp: string) => {
+    const date = new Date(timestamp);
+    const formattedDate = `${String(date.getDate()).padStart(2, '0')}-${String(date.getMonth() + 1).padStart(2, '0')}-${date.getFullYear()}`;
+    const formattedTime = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
+
+    return {
+        date: formattedDate,
+        time: formattedTime,
+    }
 }
 
-const data = [
-    {
-        id: 'out1223',
-        type: 'in',
-        amount: '12.000.000',
-        desc: 'Uang masuk dari bapak kris, Uang masuk dari bapak kris, Uang masuk dari bapak kris,Uang masuk dari bapak kris Uang masuk dari bapak kris',
-        created_at: '12 Januari 2025'
-    },
-    {
-        id: 'out12123',
-        type: 'out',
-        amount: '12.000.000',
-        desc: 'Uang masuk dari bapak kris',
-        created_at: '12 Januari 2025'
-    },
-    {
-        id: 'out112323',
-        type: 'out',
-        amount: '12.000.000',
-        desc: 'Uang masuk dari bapak kris',
-        created_at: '12 Januari 2025'
-    },
-    {
-        id: 'ou233t123',
-        type: 'out',
-        amount: '12.000.000',
-        desc: 'Uang masuk dari bapak kris',
-        created_at: '12 Januari 2025'
-    },
-]
+const dateStartEndFormat = () => {
+    
+    const end_date   = new Date();
+    const start_date = new Date(new Date().setDate(end_date.getDate() - 30));
 
-export default function Report() {
+    const formattedStartDate = `${start_date.getFullYear()}-${String(start_date.getMonth() + 1).padStart(2, '0')}-${String(start_date.getDate()).padStart(2, '0')} ` +
+                     `${String(start_date.getHours()).padStart(2, '0')}:${String(start_date.getMinutes()).padStart(2, '0')}:${String(start_date.getSeconds()).padStart(2, '0')}.` +
+                     `${String(start_date.getMilliseconds()).padStart(3, '0')}+00`;
+
+    const formattedEndDate = `${end_date.getFullYear()}-${String(end_date.getMonth() + 1).padStart(2, '0')}-${String(end_date.getDate()).padStart(2, '0')} ` +
+                     `${String(end_date.getHours()).padStart(2, '0')}:${String(end_date.getMinutes()).padStart(2, '0')}:${String(end_date.getSeconds()).padStart(2, '0')}.` +
+                     `${String(end_date.getMilliseconds()).padStart(3, '0')}+00`;
+
+    return {
+        start: formattedStartDate,
+        end: formattedEndDate,
+    }
+}
+
+type Filters = {
+    start_date: string;
+    end_date: string;
+};
+
+type Transaction = {
+    id          : string;
+    created_at   : string;
+    updated_at   : null;
+    description : string;
+    amount      : number;
+    type        : string;
+}
+
+
+const icon: Record<string, JSX.Element> = {
+    outcome: <ArrowUpCircleIcon className='w-8 h-8 text-red-400' />,
+    income: <ArrowDownCircleIcon className='w-8 h-8 text-green-300' />,
+}
+
+
+export default function Report({canDelete = false, isRefetch = false}) {
+
+    const [isFetchDataLoading, setIsFetchDataLoading] = useState(false);
+    const [isPostDataLoading, setIsPostDataLoading] = useState(false);
+    const [filters, setFilters] = useState<Filters>({
+        start_date: "",
+        end_date: "",
+    });
+    const [trxData, setTrxData] = useState<Transaction[]>([]);
+    const [error, setError] = useState<string | null>(null);
+
+
+    const onDeleteHandler = async (id: string) => {
+
+        setIsPostDataLoading(true);
+        setError(null);
+
+        const response = await fetch(`/api/transactions/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        const result = await response.json();
+        setIsPostDataLoading(false)
+
+        if (!response.ok) {
+            setError(result.message || 'An error occurred');
+        }
+    }
+
+    useEffect(() => {
+
+        const fetchData = async () => {
+            try {
+                setIsFetchDataLoading(true);
+
+                const params = new URLSearchParams({
+                    start_date: filters.start_date.substring(0, 10) + dateStartEndFormat().start.slice(10),
+                    end_date: filters.end_date.substring(0, 10) + dateStartEndFormat().end.slice(10),
+                });
+                const response = await fetch(`/api/transactions?${params.toString()}`, {
+                    method: 'GET',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    }
+                });
+
+                const result = await response.json();
+
+                if (!response.ok) {
+                    setError(result.message || 'An error occurred');
+                } else {
+                    setTrxData(result.data ?? []);
+                }
+            } catch (error) {
+                console.error('Error fetching data:', error); // Logging the error
+                setError('Failed to fetch transaction data');
+            } finally {
+                setIsFetchDataLoading(false);
+            }
+        };
+
+        if (filters.start_date && filters.end_date && !isPostDataLoading) {
+            fetchData();
+        }
+
+    }, [filters, isPostDataLoading, isRefetch])
+
+    useEffect(() => {
+        const date = dateStartEndFormat()
+        setFilters({
+            start_date: date.start,
+            end_date: date.end,
+        })
+    }, [])
+
     return (
-        <div className="px-6 lg:px-80" id="report">
-            <h2 className="mt-2 text-pretty text-3xl font-semibold tracking-tight leading-tight text-gray-900 sm:text-4xl mb-4">
+        <div className="px-6 container mt-8 mx-auto">
+            <h2 className="mt-2 text-pretty text-center text-3xl font-semibold tracking-tight leading-tight text-gray-900 sm:text-2xl mb-4">
                 Laporan Saldo Infaq Masjid
             </h2>
-            <ul role="list" className="divide-y divide-gray-100">
-                {data.map((item) => (
-                    <li key={item.id} className="flex justify-between gap-x-6 py-5">
-                        <div className="flex min-w-0 gap-x-4">
-                            <div className='w-100'>
-                                {icon[item.type]}
-                            </div>
-                            <div className="min-w-0 flex-auto">
-                                <p className="text-sm/6 font-semibold text-gray-900">{item.amount}</p>
-                                <p className="mt-1 text-xs/5 text-gray-500">{item.desc}</p>
-                            </div>
-                        </div>
-                        <div className="hidde shrink-0 sm:flex sm:flex-col sm:items-end">
-                            <p className="text-sm/6 text-gray-500 text-right">
-                                {item.created_at}<br />
-                                <span className='text-sm'>{'13:00 AM'}</span>
-                            </p>
-                        </div>
-                    </li>
-                ))}
-            </ul>
+            <div className="flex gap-5 mb-6">
+                <input
+                    onChange={ev => setFilters(state => ({ ...state, start_date: ev.target.value }))}
+                    defaultValue={filters.start_date.substring(0, 10)}
+                    id="start_date"
+                    name="start_date"
+                    type="date"
+                    className="ring-1 ring-gray-300 rounded-md outline outline-1 -outline-offset-1 outline-gray-300 block min-w-0 grow py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6 font-bold"
+                />
+                <input
+                    onChange={ev => setFilters(state => ({ ...state, end_date: ev.target.value }))}
+                    defaultValue={filters.end_date.substring(0, 10)}
+                    id="end_date"
+                    name="end_date"
+                    type="date"
+                    className="ring-1 ring-gray-300 rounded-md outline outline-1 -outline-offset-1 outline-gray-300 block min-w-0 grow py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6 font-bold"
+                />
+            </div>
+            <div className="max-h-[600px] overflow-y-scroll sm:px-4">
+                {
+                    isFetchDataLoading && !error
+                        ? <p className="animate-bounce text-gray-500 text-center font-bold">Loading...</p>
+                        : <ul role="list" className="divide-y divide-gray-100">
+                            {trxData.map((item) => (
+                                <li key={item.id} className="flex justify-between gap-x-6 py-5">
+                                    <div className="flex min-w-0 gap-x-4">
+                                        <div className='w-100'>
+                                            {icon[item.type]}
+                                        </div>
+                                        <div className="min-w-0 flex-auto">
+                                            <p className={`text-md font-bold text-gray-900 ${item.type === 'income' ? 'text-green-600' : 'text-red-600'}`}>
+                                                {item.type === 'income' ? '+' : '-'} {new Intl.NumberFormat('id-ID').format(item.amount)}
+                                            </p>
+                                            <p className="mt-1 text-xs/5 text-gray-500">{item.description}</p>
+                                        </div>
+                                    </div>
+                                    <div className="shrink-0 sm:flex sm:flex-col sm:items-end">
+                                        <p className="text-sm/6 text-gray-500 text-right">
+                                            {dateFormat(item.created_at).date}<br />
+                                            <span className='text-sm'>{dateFormat(item.created_at).time}</span>
+                                        </p>
+                                        {
+                                            canDelete &&
+                                                <div className="flex justify-end mt-2">
+                                                    <div 
+                                                        onClick={() => onDeleteHandler(item.id)}
+                                                        className="bg-red-200 rounded-full w-8 h-8 flex justify-center items-center cursor-pointer hover:bg-red-400 hover:text-white text-red-600">
+                                                        <TrashIcon className="w-5 h-5"/>
+                                                    </div>
+                                                </div>
+                                        }
+                                    </div>
+                                </li>
+                            ))}
+                        </ul>
+                }
+            </div>
         </div>
     )
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,53 +1,17 @@
 'use client'
 
-import { ArrowDownCircleIcon, ArrowUpCircleIcon, TrashIcon, HomeIcon } from "@heroicons/react/20/solid";
+import { HomeIcon } from "@heroicons/react/20/solid";
 import { useCookies } from "next-client-cookies";
 import { redirect } from "next/navigation";
 import { RedirectType } from "next/navigation";
-import { JSX, useEffect, useState } from "react";
-
-const dateFormat = (timestamp: string) => {
-    const date = new Date(timestamp);
-    const formattedDate = `${String(date.getDate()).padStart(2, '0')}-${String(date.getMonth() + 1).padStart(2, '0')}-${date.getFullYear()}`;
-    const formattedTime = `${String(date.getHours()).padStart(2, '0')}:${String(date.getMinutes()).padStart(2, '0')}`;
-
-    return {
-        date: formattedDate,
-        time: formattedTime,
-    }
-}
-
-type Filters = {
-    start_date: string;
-    end_date: string;
-};
-type Transaction = {
-    id          : string;
-    created_at   : string;
-    updated_at   : null;
-    description : string;
-    amount      : number;
-    type        : string;
-}
-
-
-const icon: Record<string, JSX.Element> = {
-    outcome: <ArrowUpCircleIcon className='w-8 h-8 text-red-400' />,
-    income: <ArrowDownCircleIcon className='w-8 h-8 text-green-300' />,
-}
-
+import { useState } from "react";
+import Report from "../components/report";
 
 export default function Dashboard() {
 
-    const [trxData, setTrxData] = useState<Transaction[]>([]);
     const [amount, setAmount] = useState('');
     const [description, setDescription] = useState('');
     const [isPostDataLoading, setIsPostDataLoading] = useState(false);
-    const [isFetchDataLoading, setIsFetchDataLoading] = useState(false);
-    const [filters, setFilters] = useState<Filters>({
-        start_date: "",
-        end_date: "",
-    });
     const [error, setError] = useState<string | null>(null);
 
     const cookies = useCookies()
@@ -112,81 +76,6 @@ export default function Dashboard() {
             setIsPostDataLoading(false);
         }
     }
-
-    const onDeleteHandler = async (id: string) => {
-
-        setIsPostDataLoading(true);
-        setError(null);
-
-        const response = await fetch(`/api/transactions/${id}`, {
-            method: 'DELETE',
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        })
-
-        const result = await response.json();
-        setIsPostDataLoading(false)
-
-        if (!response.ok) {
-            setError(result.message || 'An error occurred');
-        }
-    }
-
-
-    useEffect(() => {
-
-        const fetchData = async () => {
-            try {
-                setIsFetchDataLoading(true);
-                setError(null);
-    
-                const params = new URLSearchParams(filters);
-                const response = await fetch(`/api/transactions?${params.toString()}`, {
-                    method: 'GET',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    }
-                });
-    
-                const result = await response.json();
-    
-                if (!response.ok) {
-                    setError(result.message || 'An error occurred');
-                } else {
-                    setTrxData(result.data ?? []);
-                }
-            } catch (error) {
-                console.error('Error fetching data:', error); // Logging the error
-                setError('Failed to fetch transaction data');
-            } finally {
-                setIsFetchDataLoading(false);
-            }
-        };
-
-        if (filters.start_date && filters.end_date && !isPostDataLoading) {
-            fetchData();
-        }
-
-    }, [filters, isPostDataLoading])
-
-    useEffect(() => {
-        const end_date   = new Date();
-        const start_date = new Date(new Date().setDate(end_date.getDate() - 30));
-
-        const formattedStartDate = `${start_date.getFullYear()}-${String(start_date.getMonth() + 1).padStart(2, '0')}-${String(start_date.getDate()).padStart(2, '0')} ` +
-                         `${String(start_date.getHours()).padStart(2, '0')}:${String(start_date.getMinutes()).padStart(2, '0')}:${String(start_date.getSeconds()).padStart(2, '0')}.` +
-                         `${String(start_date.getMilliseconds()).padStart(3, '0')}+00`;
-
-        const formattedEndDate = `${end_date.getFullYear()}-${String(end_date.getMonth() + 1).padStart(2, '0')}-${String(end_date.getDate()).padStart(2, '0')} ` +
-                         `${String(end_date.getHours()).padStart(2, '0')}:${String(end_date.getMinutes()).padStart(2, '0')}:${String(end_date.getSeconds()).padStart(2, '0')}.` +
-                         `${String(end_date.getMilliseconds()).padStart(3, '0')}+00`;
-
-        setFilters({
-            start_date: formattedStartDate,
-            end_date: formattedEndDate,
-        })
-    }, [])
 
     return (
         <div className="container m-auto pt-6 max-w-xl px-4">
@@ -260,65 +149,7 @@ export default function Dashboard() {
                 </div>
             </div>
             <hr />
-            <div className=" mt-8">
-                <h2 className="mt-2 text-pretty text-center text-3xl font-semibold tracking-tight leading-tight text-gray-900 sm:text-2xl mb-4">
-                    Laporan Saldo Infaq Masjid
-                </h2>
-                <div className="flex gap-5 mb-6">
-                    <input
-                        onChange={ev => setFilters(state => ({ ...state, start_date: ev.target.value }))}
-                        defaultValue={filters.start_date.substring(0, 10)}
-                        id="start_date"
-                        name="start_date"
-                        type="date"
-                        className="ring-1 ring-gray-300 rounded-md outline outline-1 -outline-offset-1 outline-gray-300 block min-w-0 grow py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6 font-bold"
-                    />
-                    <input
-                        onChange={ev => setFilters(state => ({ ...state, end_date: ev.target.value }))}
-                        defaultValue={filters.end_date.substring(0, 10)}
-                        id="end_date"
-                        name="end_date"
-                        type="date"
-                        className="ring-1 ring-gray-300 rounded-md outline outline-1 -outline-offset-1 outline-gray-300 block min-w-0 grow py-1.5 px-3 text-base text-gray-900 placeholder:text-gray-400 focus:outline focus:outline-0 sm:text-sm/6 font-bold"
-                    />
-                </div>
-                <div className="max-h-[600px] overflow-y-scroll sm:px-4">
-                {
-                    isFetchDataLoading 
-                        ? <p className="animate-bounce text-gray-500 text-center font-bold">Loading...</p>
-                        : <ul role="list" className="divide-y divide-gray-100">
-                            {trxData.map((item) => (
-                                <li key={item.id} className="flex justify-between gap-x-6 py-5">
-                                    <div className="flex min-w-0 gap-x-4">
-                                        <div className='w-100'>
-                                            {icon[item.type]}
-                                        </div>
-                                        <div className="min-w-0 flex-auto">
-                                            <p className={`text-md font-bold text-gray-900 ${item.type === 'income'? 'text-green-600': 'text-red-600'}`}>
-                                            {item.type === 'income'? '+': '-'} { new Intl.NumberFormat('id-ID').format(item.amount)}
-                                            </p>
-                                            <p className="mt-1 text-xs/5 text-gray-500">{item.description}</p>
-                                        </div>
-                                    </div>
-                                    <div className="shrink-0 sm:flex sm:flex-col sm:items-end">
-                                        <p className="text-sm/6 text-gray-500 text-right">
-                                            {dateFormat(item.created_at).date}<br />
-                                            <span className='text-sm'>{dateFormat(item.created_at).time}</span>
-                                        </p>
-                                        <div className="flex justify-end mt-2">
-                                            <div 
-                                                onClick={() => onDeleteHandler(item.id)}
-                                                className="bg-red-200 rounded-full w-8 h-8 flex justify-center items-center cursor-pointer hover:bg-red-400 hover:text-white text-red-600">
-                                                <TrashIcon className="w-5 h-5"/>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                }
-                </div>
-            </div>
+            <Report canDelete={true} isRefetch={isPostDataLoading}/>
         </div>
     )
 }
