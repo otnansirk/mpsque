379126c778915890f33eb9e04454ded97753c149
otnansirk <iam.otnansirk@gmail.com>
2025-01-11 13:25:05 +0700

    rename file

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import About from "./components/about";
 import Features from "./components/features";
 import Footer from "./components/footer";
 import Hero from "./components/hero";
-import Quote from "./components/Quote";
+import Quote from "./components/quote";
 import Report from "./components/report";
 
 export default function Home() {
