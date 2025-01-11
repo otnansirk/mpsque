ff10fd1aa2b4a2ee31651cb5ba21c256cb3b520c
otnansirk <iam.otnansirk@gmail.com>
2025-01-11 20:10:36 +0700

    fix : add persistant session

--- src/libs/api.ts
+++ src/libs/api.ts
@@ -2,7 +2,14 @@ import { createClient, SupabaseClient } from "@supabase/supabase-js";
 
 export default function ClientAPI(): SupabaseClient | null {
     if (process.env.SUPABASE_URL && process.env.SUPABASE_API_KEY) {
-        const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_API_KEY)
+        const options = {
+            auth: {
+                persistSession: true,
+                autoRefreshToken: true,
+            },
+        }
+
+        const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_API_KEY, options)
         return supabase
     }
     return null
