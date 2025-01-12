12be9593b2da7ad0f93ee1c0f8809eb80211f40c
otnansirk <iam.otnansirk@gmail.com>
2025-01-12 10:43:53 +0700

    move runtime to nodejs

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone",  // Pastikan output diatur dengan benar untuk non-Edge mode.
+  runtime: "nodejs",
   env: {
     
   }
